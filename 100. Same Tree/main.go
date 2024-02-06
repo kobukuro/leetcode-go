0867f38bae87b4bd1c2217a029cb2d1751bd1ca5
PeterChen <sanpeter992000@gmail.com>
2024-02-06 23:03:33 +0800

    100. Same Tree

---
+++ 100. Same Tree/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p != nil && q == nil {
+		return false
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+func main() {
+	p := &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}}
+	q := &TreeNode{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}}
+	res := isSameTree(p, q)
+	fmt.Println(res)
+}
