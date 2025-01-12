7503d37f9d71862cc3a89208380d058722096fda
PeterChen <sanpeter992000@gmail.com>
2025-01-12 11:05:13 +0800

    226. Invert Binary Tree

---
+++ 226. Invert Binary Tree/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	tempLeft := root.Left
+	root.Left = root.Right
+	root.Right = tempLeft
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+func main() {
+	root := &TreeNode{Val: 4, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 7, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 9}}}
+	fmt.Println(invertTree(root))
+}
