6b829fbeeb4d24b24613519d5495e163d631fe55
PeterChen <sanpeter992000@gmail.com>
2025-01-14 23:16:19 +0800

    572. Subtree of Another Tree

---
+++ 572. Subtree of Another Tree/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil && q != nil {
+		return false
+	}
+	if p != nil && q == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if subRoot == nil {
+		return true
+	}
+	if root == nil {
+		return false
+	}
+	if isSameTree(root, subRoot) {
+		return true
+	}
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+func main() {
+	root := &TreeNode{Val: 3, Left: &TreeNode{Val: 4, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 2}}, Right: &TreeNode{Val: 5}}
+	subRoot := &TreeNode{Val: 4, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 2}}
+	fmt.Println(isSubtree(root, subRoot)) // true
+}
